f135aeaa37aaa88ad95e1bae27177c700b557080
Trung <trunghere@gmail.com>
2026-06-29 13:44:15 +0700

    fix: update PWA manifest for maskable icon

--- vite.config.ts
+++ vite.config.ts
@@ -52,14 +52,16 @@ export default defineConfig(() => ({
         display: "standalone",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "/fh.github.io/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
+            purpose: "any",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/fh.github.io/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any maskable",
           },
         ],
       },
